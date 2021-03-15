a4d1642ee330b250320df5b9c982b9fed30675d6
Fernando Cardenas <fernando.cardenasm@gmail.com>
2021-03-15 10:02:27 +0100

    Insert delivers NoError On EmptyCache and also on non empty cache

--- Tests/FeedStoreChallengeTests.swift
+++ Tests/FeedStoreChallengeTests.swift
@@ -47,15 +47,15 @@ class FeedStoreChallengeTests: XCTestCase, FeedStoreSpecs {
 	}
 	
 	func test_insert_deliversNoErrorOnEmptyCache() throws {
-//		let sut = try makeSUT()
-//
-//		assertThatInsertDeliversNoErrorOnEmptyCache(on: sut)
+		let sut = try makeSUT()
+
+		assertThatInsertDeliversNoErrorOnEmptyCache(on: sut)
 	}
 	
 	func test_insert_deliversNoErrorOnNonEmptyCache() throws {
-//		let sut = try makeSUT()
-//
-//		assertThatInsertDeliversNoErrorOnNonEmptyCache(on: sut)
+		let sut = try makeSUT()
+
+		assertThatInsertDeliversNoErrorOnNonEmptyCache(on: sut)
 	}
 	
 	func test_insert_overridesPreviouslyInsertedCacheValues() throws {
